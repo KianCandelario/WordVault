f31dd056369072b3907042261769811e5d5052fa
Kian Candelario <kiancandelario1@gmail.com>
2023-06-30 21:31:00 +0800

    completed the functionality, except the sample sentence and error handlings

---
+++ js/app.js
@@ -0,0 +1,44 @@
+// Setting variables
+const wordInput = document.querySelector('input');
+const bttn = document.querySelector('#search-bttn');
+const desiredWord = document.querySelector('#word');
+const posPhon = document.querySelector('#pos-phon');
+const wordDef = document.querySelector('.word-meaning');
+const audio = document.querySelector('#speaker-icon');
+const audioTag = document.querySelector('#speaker-audio');
+const container = document.querySelector('.modal');
+const wrapper = document.querySelector('.definition-wrapper');
+const errorDiv = document.querySelector('.error');
+
+// Fetching API
+async function fetchAPI(word) {
+    const apiRequest = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+    const api = await apiRequest.json();
+    return(api);
+}
+
+bttn.addEventListener('click', async () => {
+    try {
+        const word = wordInput.value;
+        const apiData = await fetchAPI(word);
+
+        const phonetics = apiData[0].phonetics[0].text;
+        const pos = apiData[0].meanings[0].partOfSpeech;
+        const definition = apiData[0].meanings[0].definitions[0].definition;
+        const audioSrc = apiData[0].phonetics[0].audio;
+        
+        desiredWord.innerHTML = word;
+        posPhon.innerHTML = pos + "  &#x2022  " + phonetics;
+        wordDef.innerHTML = definition;
+        audioTag.setAttribute('src', audioSrc);
+
+        wordInput.value = '';
+    } catch {
+        wordInput.value = '';
+    }
+
+});
+
+audio.addEventListener('click', () => {
+    audioTag.play();
+});
